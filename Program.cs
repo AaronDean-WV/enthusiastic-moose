32bf7e34008fa80f8aa82b4b3aba3db7ede12b1d
Aaron Dean <adean30462@gmail.com>
2023-05-23 14:18:09 -0400

    moose done

--- Program.cs
+++ Program.cs
@@ -1,28 +1,34 @@
-﻿using System;
+﻿
+using System;
 
-Main();
-
- void Main()
+class Program
 {
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+        Console.WriteLine("--------------------------------------------");
+        Console.WriteLine();
 
-    Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
-    Console.WriteLine("--------------------------------------------");
-    Console.WriteLine();
+        // Let the moose speak!
+        MooseSays("Ask me a question:");
+        // MooseSays("I really am enthusiastic");
 
-    // Let the moose speak!
-    MooseSays("H I, I'M  E N T H U S I A S T I C !");
-    MooseSays("I really am enthusiastic");
+        while (true)
+        {
+            // Console.Write("Ask me a question: ");
+            string question = Console.ReadLine();
 
-    // As a question
-    CanadaQuestion();
-    EnthusiasticQuestion();
-    LoveCSharpQuestion();
-    SecretQuestion();
-}
+            if (string.IsNullOrWhiteSpace(question))
+                break;
 
-void MooseSays(string message)
-{
-    Console.WriteLine($@"
+            string response = GetRandomResponse();
+            MooseSays(response);
+        }
+    }
+
+    static void MooseSays(string message)
+    {
+        Console.WriteLine($@"
                                        _.--^^^--,
                                     .'          `\
   .-^^^^^^-.                      .'              |
@@ -50,76 +56,34 @@ void MooseSays(string message)
                       \ \'._  ` '_.'
                        `^^` `^^^`
     ");
-}
-
-
-bool MooseAsks(string question)
-{
-    Console.Write($"{question} (Y/N): ");
-    string answer = Console.ReadLine().ToLower();
-
-    while (answer != "y" && answer != "n")
-    {
-        Console.Write($"{question} (Y/N): ");
-        answer = Console.ReadLine().ToLower();
     }
 
-    if (answer == "y")
+    static string GetRandomResponse()
     {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-void CanadaQuestion()
-{
-    bool isTrue = MooseAsks("Is Canada real?");
-    if (isTrue)
-    {
-        MooseSays("Really? It seems very unlikely.");
-    }
-    else
-    {
-        MooseSays("I  K N E W  I T !!!");
-    }
-}
-void EnthusiasticQuestion()
-{
-    bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
-    if (isEnthusiastic)
-    {
-        MooseSays("Yay!");
-    }
-    else
-    {
-        MooseSays("You should try it!");
-    }
-}
-
-void LoveCSharpQuestion()
-{
-    bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
-    if (doesLoveCSharp)
-    {
-        MooseSays("Good job sucking up to your instructor!");
-    }
-    else
-    {
-        MooseSays("You will...oh, yes, you will...");
-    }
-}
-
-void SecretQuestion()
-{
-    bool wantsSecret = MooseAsks("Do you want to know a secret?");
-    if (wantsSecret)
-    {
-        MooseSays("ME TOO!!!! I love secrets...tell me one!");
-    }
-    else
-    {
-        MooseSays("Oh, no...secrets are the best, I love to share them!");
+        Random random = new Random();
+        string[] responses = {
+            "As I see it, yes.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don’t count on it.",
+            "It is certain.",
+            "It is decidedly so.",
+            "Most likely.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Outlook good.",
+            "Reply hazy, try again.",
+            "Signs point to yes.",
+            "Very doubtful.",
+            "Without a doubt.",
+            "Yes.",
+            "Yes – definitely.",
+            "You may rely on it."
+        };
+        int index = random.Next(responses.Length);
+        return responses[index];
     }
 }
